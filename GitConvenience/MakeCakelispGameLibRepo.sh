cd5f633f75d6862800d4746e238cb4fbcd97eca6
Macoy Madson <macoy@macoy.me>
2021-11-16 09:06:40 -0500

    Fix repo builder to use submodules

--- GitConvenience/MakeCakelispGameLibRepo.sh
+++ GitConvenience/MakeCakelispGameLibRepo.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+git init
+
 mkdir src
 mkdir Dependencies
-cd Dependencies
-git clone git@github.com:makuto/cakelisp.git
-git clone gitea@macoy.me:macoy/gamelib.git
-cd ../
+git submodule add git@github.com:makuto/cakelisp.git Dependencies/cakelisp
+git submodule add gitea@macoy.me:macoy/gamelib.git Dependencies/gamelib
 
 cp Dependencies/cakelisp/COPYING .
 cp Dependencies/cakelisp/LICENSE .
 touch ReadMe.org
+
+git add .
+git commit -m"Initial commit"
